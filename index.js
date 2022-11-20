dc1c7a8584015efb1aaeda08d6e9db134b4825ac
sylvie-vongsak <vongsak.v@gmail.com>
2022-11-20 22:52:17 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/home", async (req, res) => {
+  try {
+    const limit = req.query.limit || 100;
+    const skip = req.query.skip || 0;
+    const name = req.query.name || "";
+
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.MARVEL_API_KEY}&limit=${limit}&skip=${skip}&name=${name}`
+    );
+
+    res.json(response.data);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+app.get("/comics", async (req, res) => {
+  try {
+    const limit = req.query.limit || 100;
+    const skip = req.query.skip || 0;
+    const title = req.query.title || "";
+
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.MARVEL_API_KEY}&limit=${limit}&skip=${skip}&title=${title}`
+    );
+
+    res.json(response.data);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+app.get("/character", async (req, res) => {
+  try {
+    const id = req.query.id;
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comics/${id}?apiKey=${process.env.MARVEL_API_KEY}`
+      // `https://lereacteur-marvel-api.herokuapp.com/character/${id}?apiKey=${process.env.MARVEL_API_KEY}`
+    );
+
+    https: res.json(response.data);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+app.all("*", (req, res) => {
+  res.status(404).json({ message: "This route doesn't exist" });
+});
+
+app.listen(4000, () => {
+  console.log("Server started");
+});
