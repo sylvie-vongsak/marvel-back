1667ac696abc89c90d795d29449698c4440f69fd
sylvie-vongsak <vongsak.v@gmail.com>
2022-11-22 13:26:53 +0100

    dotenv

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
-require("dotenv").config();
 
 const app = express();
 app.use(express.json());
